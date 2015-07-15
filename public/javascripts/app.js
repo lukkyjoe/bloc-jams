1a0514171b99aad9229dd02f883f61395c0e5f3d
Joseph Luk <joseph.sc.luk@gmail.com>
2015-07-15 05:24:58 +0000

    Converted album view to Angular

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -292,6 +292,11 @@ var blocJams = angular.module('BlocJams', ['ui.router']);
      controller: 'Collection.controller',
      templateUrl: '/templates/collection.html'
    });
+   $stateProvider.state('album', {
+     url: '/album',
+     templateUrl: '/templates/album.html',
+     controller: 'Album.controller'
+   });
    $stateProvider.state('landing', {
      url: '/',
      controller: 'Landing.controller',
@@ -325,6 +330,38 @@ var blocJams = angular.module('BlocJams', ['ui.router']);
      for (var i = 0; i < 33; i++){
        $scope.albums.push(angular.copy(albumPicasso));
      }
+ }]);
+ blocJams.controller('Album.controller', ['$scope', function($scope) {
+   $scope.album = angular.copy(albumPicasso);
+   
+      var hoveredSong = null;
+   var playingSong = null;
+ 
+   $scope.onHoverSong = function(song) {
+     hoveredSong = song;
+   };
+ 
+   $scope.offHoverSong = function(song) {
+     hoveredSong = null;
+   };
+   
+      $scope.getSongState = function(song) {
+     if (song === playingSong) {
+       return 'playing';
+     }
+     else if (song === hoveredSong) {
+       return 'hovered';
+     }
+     return 'default';
+   };
+   
+       $scope.playSong = function(song) {
+      playingSong = song;
+    };
+ 
+    $scope.pauseSong = function(song) {
+      playingSong = null;
+    };
  }]);
 // angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
 //   $scope.subText = "yooo!";
